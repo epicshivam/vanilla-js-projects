3d2a9e3c6bb5f860d4626dd8f9f8756823b57401
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-08-14 01:10:35 +0530

    added function

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,14 +1,28 @@
 import { API_KEY } from "./config.js";
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${API_KEY}`;
+// const url = ``;
+
+const inputEl = document.querySelector("input");
+const weatherDiv = document.querySelector('weatherResult');
 
 
-const fetchWeather = async ()=> {
-    const response = await fetch(url);
+inputEl.addEventListener("input", (e)=> {
+    console.log(`Entered city is : ${e.target.value}`);
+    fetchWeather(e.target.value);
+})
+
+const fetchWeather = async (city)=> {
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.toUpperCase()}&appid=${API_KEY}`);
     const data = await response.json();
     console.log(data);
+    updateResult(data);
 }
 
 
-
-fetchWeather();
+const updateResult = (data) => {
+    weatherDiv.innerHTML = `
+    
+    <p>${data.name}</p>
+    <p>${data.main.temp}</p>
+    `;
+}
