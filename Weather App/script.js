26ed4a51352801931756ff9d710c184322694dc0
Shivam Singh <shivamsinghbsr0@gmail.com>
2025-08-14 23:18:22 +0530

    fixed bugs

--- Weather App/script.js
+++ Weather App/script.js
@@ -1,18 +1,26 @@
 import { API_KEY } from "./config.js";
 
-// const url = ``;
-
 const inputEl = document.querySelector("input");
-const weatherDiv = document.querySelector('weatherResult');
+const weatherDiv = document.querySelector('.weatherResult');
+const searchIcon = document.querySelector("#searchIcon");
+
+searchIcon.addEventListener("click", (e) => {
+    e.preventDefault();
+    const city = inputEl.value.trim().toUpperCase();
+    if (city) fetchWeather(city);
+});
 
 
-inputEl.addEventListener("input", (e)=> {
-    console.log(`Entered city is : ${e.target.value}`);
-    fetchWeather(e.target.value);
+inputEl.addEventListener("keypress", (e)=> {
+    if(e.key==="Enter"){
+        e.preventDefault();
+        console.log(`Entered city is : ${e.target.value}`);
+        fetchWeather(e.target.value);
+    }
 })
 
 const fetchWeather = async (city)=> {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.toUpperCase()}&appid=${API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city.toUpperCase()}&appid=${API_KEY}&units=metric`);
     const data = await response.json();
     console.log(data);
     updateResult(data);
@@ -20,9 +28,14 @@ const fetchWeather = async (city)=> {
 
 
 const updateResult = (data) => {
+     if (data.cod !== 200) {
+        weatherDiv.innerHTML = `<p>${data.message}</p>`;
+        return;
+    }
     weatherDiv.innerHTML = `
     
     <p>${data.name}</p>
     <p>${data.main.temp}</p>
+    <p>${data.weather[0].description}</p>
     `;
 }
